16308c5cc5873535fb81f453852194dbed162564
jarltuxen <jarl.tuxen@gmail.com>
2024-09-02 16:13:01 +0200

    prep postman

--- src/main/java/com/example/jpastudentprep2/controller/StudentRestController.java
+++ src/main/java/com/example/jpastudentprep2/controller/StudentRestController.java
@@ -2,13 +2,14 @@ package com.example.jpastudentprep2.controller;
 
 import com.example.jpastudentprep2.model.Student;
 import com.example.jpastudentprep2.repositories.StudentRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentRestController {
@@ -40,4 +41,51 @@ public class StudentRestController {
         return studentRepository.findByName(name);
 
     }
+
+    @PostMapping("/student")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student addstudent(@RequestBody Student student) {
+        System.out.println(student);
+        return studentRepository.save(student);
+    }
+
+    @PutMapping("/student/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable int id, @RequestBody Student student) {
+        Optional<Student> studentOptional = studentRepository.findById(id);
+        if (studentOptional.isPresent()) {
+            studentRepository.save(student);
+            return ResponseEntity.ok(student);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/student/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable int id) {
+        Optional<Student> studentOptional = studentRepository.findById(id);
+        if (studentOptional.isPresent()) {
+            studentRepository.deleteById(id);
+            return ResponseEntity.ok("Student deleted successfully");
+        }
+        else {
+            //return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found");
+        }
+    }
+
+    @GetMapping("/student/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable int id) {
+        Optional<Student> studentOptional = studentRepository.findById(id);
+        if (studentOptional.isPresent()) {
+
+            return ResponseEntity.ok(studentOptional.get());
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+
 }
